0b59c32e01d15f1148fe4958c841c59eaf161c39
Nora IBOUDGHACEM <niboudghacem@b110-56.(none)>
2013-01-28 16:31:44 +0100

    ajout

--- app/controllers/Trajets.java
+++ app/controllers/Trajets.java
@@ -10,12 +10,15 @@ import models.*;
 public class Trajets extends Controller {
 
     public static void index() {
-		List<Trajet> trajetsDisponibles = Trajet.findAll();
+		List<Trajet> trajetsDisponibles = Trajet.find("byIsDispo",true).fetch();
 		render(trajetsDisponibles);
     }
     
     public static void demander(Long id){
-    	
+    	Trajet trajetReserve = Trajet.findById(id);
+    	trajetReserve.isDispo = false;
+    	trajetReserve.save();
+    	index();
     }
 
 }
