d915bcbb51dbfb56636d3bd31e963c1a2cc84d59
Nora IBOUDGHACEM <niboudghacem@b110-56.(none)>
2013-01-28 14:51:36 +0100

    toto

--- Covoiturage-GB/app/models/Trajet.java
+++ Covoiturage-GB/app/models/Trajet.java
@@ -1,16 +1,24 @@
 package models;
 
+import javax.persistence.Entity;
+import javax.persistence.ManyToOne;
+import javax.persistence.OneToMany;
+import javax.persistence.OneToOne;
+
 import play.db.jpa.Model;
 
+@Entity
 public class Trajet extends Model{
 	
-	public String Ville_Depart;
-	public String Ville_Arrive;
+	public String VilleDepart;
+	public String VilleArrive;
+	
+	@ManyToOne
 	public User Conducteur;
 	
-	public Trajet(String Ville_Depart, String Ville_Arrive, User Conducteur) {
-	        this.Ville_Depart = Ville_Depart;
-	        this.Ville_Arrive = Ville_Arrive;
+	public Trajet(String VilleDepart, String VilleArrive, User Conducteur) {
+	        this.VilleDepart = VilleDepart;
+	        this.VilleArrive = VilleArrive;
 	        this.Conducteur = Conducteur;
 	 }
 	
--- Covoiturage-GB/app/models/User.java
+++ Covoiturage-GB/app/models/User.java
@@ -1,7 +1,25 @@
 package models;
 
+import java.util.List;
+
+import javax.persistence.Entity;
+import javax.persistence.OneToMany;
+import javax.persistence.OneToOne;
+
 import play.db.jpa.Model;
 
+@Entity
 public class User extends Model{
 
+	public String nom;
+	public String prenom;
+
+	@OneToMany
+	public List<Trajet> trajet;
+	
+	public User(String nom, String prenom) {
+		super();
+		this.nom = nom;
+		this.prenom = prenom;
+	}	
 }
